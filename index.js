bcda6f4fc3feacf9fdfaff70a88eb9a5baeb6271
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-31 02:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveLastKitten() {
   return kittens
 }
 
-destructivelyRemoveFirstKitten() {
+function destructivelyRemoveFirstKitten() {
   kittens.shift()
   return kittens
 }
